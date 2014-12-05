55801d255e45057e60defd1393a9773024cb429e
tuanguyen94 <tuanguyen94@gmail.com>
2014-12-06 00:37:21 +0700

    edit doxygen

--- Project1_final/function.cs
+++ Project1_final/function.cs
@@ -22,14 +22,20 @@ namespace Project1_final
         /// </summary>
         public void SetDHCP()
         {
+            //Khởi tạo một lớp ManagementClass
+            //ManagementClas là 1 class có các phương thức lấy và cài đặt thông tin của thiết bị
             ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            //Khai báo 1 tập hợp các thể hiện của lớp ManagementClass được trả về từ phương thức GetInstances()
             ManagementObjectCollection moc = mc.GetInstances();
 
             foreach (ManagementObject mo in moc)
             {
-                // Make sure this is a IP enabled device. Not something like memory card or VM Ware
+                //Kiểm tra xem máy tính có cho phép thiết đặt địa chỉ IP không
+                //If TRUE, TCP/IP is bound and enabled on this network adapter.
                 if ((bool)mo["IPEnabled"])
                 {
+                    
+                        //GetMethodParameters Trả về một  1 element cơ bản đại diện cho danh sách các thông số đầu vào của phương pháp.
                         ManagementBaseObject newDNS = mo.GetMethodParameters("SetDNSServerSearchOrder");
                         newDNS["DNSServerSearchOrder"] = null;
                         ManagementBaseObject enableDHCP = mo.InvokeMethod("EnableDHCP", null, null);
@@ -50,13 +56,17 @@ namespace Project1_final
         /// <param name="Hostname">Hostname mới</param>
         public void SetIP( string IpAddresses, string SubnetMask, string Gateway, string DnsSearchOrder, string Hostname)
         {
+            //Khởi tạo một lớp ManagementClass
+            //ManagementClas là 1 class có các phương thức lấy và cài đặt thông tin của thiết bị
             ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            //Khai báo 1 tập hợp các thể hiện của lớp ManagementClass được trả về từ phương thức GetInstances()
             ManagementObjectCollection moc = mc.GetInstances();
 
             foreach (ManagementObject mo in moc)
             {
-                
-                // Make sure this is a IP enabled device. Not something like memory card or VM Ware
+
+                //Kiểm tra xem máy tính có cho phép thiết đặt địa chỉ IP không
+                //If TRUE, TCP/IP is bound and enabled on this network adapter.
                 if ((bool)mo["IPEnabled"])
                 {
                     
@@ -74,12 +84,13 @@ namespace Project1_final
 
                         newDNS["DNSServerSearchOrder"] = DnsSearchOrder.Split(',');
 
-                        
 
+                        ///Gọi một phương thức trên đối tượng WMI
+                        //Các thông số đầu vào và đầu ra được biểu diễn là đối tượng ManagementBaseObject 
                         ManagementBaseObject setIP = mo.InvokeMethod("EnableStatic", newIP, null);
                         ManagementBaseObject setGateways = mo.InvokeMethod("SetGateways", newGate, null);
                         ManagementBaseObject setDNS = mo.InvokeMethod("SetDNSServerSearchOrder", newDNS, null);
-
+                        //khai báo cơ sở dữ liệu dùng để lưu trữ thông số kỹ thuật của Windows.
                         RegistryKey key = Registry.LocalMachine.OpenSubKey("SYSTEM", true).OpenSubKey("CurrentControlSet", true).OpenSubKey("Services", true).OpenSubKey("tcpip", true).OpenSubKey("Parameters", true);
 
                         key.SetValue("Hostname", Hostname);
